7314a66d5e38a82b71056c410dd0fc9e1871cb78
ana-bia2903 <106672400+ana-bia2903@users.noreply.github.com>
2024-08-26 08:58:59 -0300

    hiib

--- js/aleatorio.js
+++
@@ -1,9 +0,0 @@
-{const nomes = ["ana beatriz"];
-    
-    export function aleatorio (lista){
-        const posicao = Math.floor(Math.random()* lista.length);
-        return lista[posicao];
-    }
-    
-    export const nome = aleatorio(nomes)
-}
